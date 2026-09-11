06ddda1fdf4819c9de11f839a98b57e17ece51d0
mubasherjam <mubasherworld@gmail.com>
2026-09-11 17:42:07 +0500

    Add project files.

---
+++ ECommerce.API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ ECommerce.API/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+    }
+}
